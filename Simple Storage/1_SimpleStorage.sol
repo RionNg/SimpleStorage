76ad6fae4f21fb50da3c75457e9f87a4b098e5ab
RionNg <147371006+RionNg@users.noreply.github.com>
2023-11-08 17:30:53 +0800

    Add files via upload

---
+++ Simple Storage/1_SimpleStorage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.22;
+
+contract SimpleStorage {
+    uint256 public myFavoriteNumber = 0;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // dynamic array
+    Person[] public listOfPeople;
+    // Person public pat = Person({favoriteNumber: 7, name: "pat"});
+    // Person public mariah = Person({favoriteNumber: 16, name: "Mariah"});
+    // Person public jon = Person({favoriteNumber: 12, name: "Jon"});
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return myFavoriteNumber;
+    }
+    
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push( Person(_favoriteNumber, _name) );
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+    
+}
---
+++ Simple Storage/2_StorageFactory.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.22;
+
+import {SimpleStorage} from "./1_SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContact() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+        // SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        // mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+        // SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        // return mySimpleStorage.retrieve();
+    }
+
+}
